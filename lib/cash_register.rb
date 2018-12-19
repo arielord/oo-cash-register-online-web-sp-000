2f7dda4ebf42b66490d51698cce3252b2cf33c97
Ariel <arielord@buffalo.edu>
2018-12-19 05:35:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
-
+class CashRegister
+  attr_accessor :total :discount
+  
+  def initialize(discount = nil)
+    self.total = 0
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_items()
+    
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+end
