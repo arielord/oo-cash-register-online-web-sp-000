3a86904b5683a9f291174ab8c8a7af9267f7962b
Ariel <arielord@buffalo.edu>
2018-12-19 05:35:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,9 @@
 class CashRegister
   attr_accessor :total, :discount
   
-  def initialize(discount = nil)
+  def initialize(discount = 0)
     self.total = 0
+    self.discount = discount
   end
   
   def total
